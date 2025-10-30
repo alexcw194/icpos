ae88803ec7f413febeb9b16d3bd60195dc81cbb4
alexcw194 <mail@inderacipta.com>
2025-10-30 21:47:38 +0700

    Update sidebar.blade.php

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -189,9 +189,16 @@
         @hasanyrole('Admin|SuperAdmin')
         <li class="nav-item">
           <a class="nav-link {{ request()->routeIs('po.*') ? 'active' : '' }}" href="{{ route('po.index') }}">
-            {{-- Icon optional, sesuaikan dengan yang lain di sidebar --}}
             <span class="nav-link-icon d-md-none d-lg-inline-block">
-              <i class="ti ti-shopping-cart"></i>
+              {{-- Tabler Icon: shopping-cart (inline SVG, konsisten dengan menu lain) --}}
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                  viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="2">
+                <path stroke="none" d="M0 0h24v24H0z"/>
+                <path d="M6 6h15l-1.5 9h-12z"/>
+                <path d="M6 6l-2 -2"/>
+                <path d="M9 20a1 1 0 1 0 0 -2a1 1 0 0 0 0 2"/>
+                <path d="M17 20a1 1 0 1 0 0 -2a1 1 0 0 0 0 2"/>
+              </svg>
             </span>
             <span class="nav-link-title">Purchase Orders</span>
           </a>
