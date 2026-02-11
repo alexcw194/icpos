564ba90ee929f72cd1518c5e01f5b953ff80501d
alexcw194 <mail@inderacipta.com>
2026-02-11 13:33:03 +0700

    Remove duplicate flash alerts from delivery show page

--- resources/views/deliveries/show.blade.php
+++ resources/views/deliveries/show.blade.php
@@ -2,15 +2,6 @@
 
 @section('content')
 <div class="container-xl">
-  @if(session('success'))
-    <div class="alert alert-success mb-3">{{ session('success') }}</div>
-  @endif
-  @if(session('error'))
-    <div class="alert alert-danger mb-3">{{ session('error') }}</div>
-  @endif
-  @if(session('info'))
-    <div class="alert alert-info mb-3">{{ session('info') }}</div>
-  @endif
   @if($errors->any())
     <div class="alert alert-danger mb-3">
       <ul class="mb-0">
