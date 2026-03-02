365203312f2851fd9b2465de0b78488007e3f2cb
alexcw194 <mail@inderacipta.com>
2026-03-02 14:30:40 +0700

    Lower ICP stamp and signature position further in BAST

--- resources/views/documents/templates/icp_bast_standard.blade.php
+++ resources/views/documents/templates/icp_bast_standard.blade.php
@@ -163,7 +163,7 @@
     .sign-stamp {
       position: absolute;
       left: 50%;
-      top: 58%;
+      top: 62%;
       transform: translate(-50%, -50%);
       max-height: 70px;
       z-index: 1;
@@ -171,7 +171,7 @@
     .sign-signature {
       position: absolute;
       left: 50%;
-      top: 58%;
+      top: 62%;
       transform: translate(-50%, -50%);
       max-height: 70px;
       z-index: 2;
